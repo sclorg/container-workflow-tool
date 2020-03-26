28dedd627b7bedad75276ae162cbb1a4f76b01ef
ankitdobhal <dobhal.ankit@protonmail.com>
2020-03-26 20:40:01 +0530

    Updated newversion

--- container_workflow_tool/newversion.py
+++ container_workflow_tool/newversion.py
@@ -1,3 +1,21 @@
-#!/usr/bin/python3
-# newversion script with the help of Gitpython python.
-from git import *
+#! /usr/bin/python3
+# newversion command
+
+import subprocess as sub
+from os import system
+from re import findall
+def create_newversion(_version):
+	system('tput setaf 2')
+	print('[*] Creating new version')
+	a=sub.call(['mkdir',_version])
+
+     
+def move_version(_oldversion,_version):
+	create_newversion()
+	b=sub.call(['cp -r ',_oldversion,_version])
+	sub.call(['rm -rf ',_oldversion,'/.git'])
+
+
+	
+
+	
