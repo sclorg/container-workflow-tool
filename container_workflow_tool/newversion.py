95c381b4c605bd3d2fc3c4cd64bd27e2248a4065
ankitdobhal <dobhal.ankit@protonmail.com>
2020-04-10 17:28:37 +0530

    Updated newversion file

--- container_workflow_tool/newversion.py
+++ container_workflow_tool/newversion.py
@@ -1,21 +1,30 @@
-#! /usr/bin/python3
-# newversion command
-
+import os
 import subprocess as sub
-from os import system
-from re import findall
-def create_newversion(_version):
-	system('tput setaf 2')
-	print('[*] Creating new version')
-	a=sub.call(['mkdir',_version])
+import shutil
 
-     
-def move_version(_oldversion,_version):
-	create_newversion()
-	b=sub.call(['cp -r ',_oldversion,_version])
-	sub.call(['rm -rf ',_oldversion,'/.git'])
+from git import Repo
+from git.exc import GitCommandError
+
+class NewVersion(object,):
+	def __init__(self,_oldversion,_version):
+		self._oldversion=_oldversion
+		self._version=_version
+
+	def create_newversion(_version):
+		system('tput setaf 2')
+	    print('[*] Creating new version')
+	    sub.call(['mkdir',self._version])
 
 
-	
+    def move_version(_oldversion,_version):
+    	create_newversion()
+	    sub.call(['git mv ',self._oldversion,self._version])
+	    sub.run(['git commit'],shell=true)
 
-	
+
+    def new_commit(_oldversion,_version):
+    	move_version()
+    	sub.call(['cp -r',self._version,self._oldversion])
+    	sub.call(['git add',self._version])
+    	sub.run(['git commit'],shell=true)
+    	
