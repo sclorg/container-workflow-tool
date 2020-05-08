79c30bf85a810f0d7d88a4b349ddf8ea315bced2
ankitdobhal <dobhal.ankit@protonmail.com>
2020-05-08 18:06:02 +0530

    Updated newversion Script

--- container_workflow_tool/newversion.py
+++ container_workflow_tool/newversion.py
@@ -1,29 +1,24 @@
 import os
 import subprocess as sub
-import shutil
+import sys
 
-from git import Repo
-from git.exc import GitCommandError
+class NewVersion(object):
+    def __init__(self,_oldversion,_version):
+    	self._oldversion=_oldversion
+    	self._version=_version
 
-class NewVersion(object,):
-	def __init__(self,_oldversion,_version):
-		self._oldversion=_oldversion
-		self._version=_version
-
-		def create_newversion(_version):
-			system('tput setaf 2')
-			sub.call(['mkdir',self._version])
+    def move(_oldversion,_version):
+    	sub.Popen(['git','mv',_oldversion,_version],stdout=sub.PIPE)
+    	sub.call('git commit -m "Added new version %s"'%_version,shell=True)
 
 
-		def move_version(_oldversion,_version):
-			create_newversion()
-			sub.call(['git mv ',self._oldversion,self._version])
-			sub.run(['git commit'],shell=true)
+    def copyadd(_oldversion,_version):
+    	sub.Popen(['cp','-r',_version,_oldversion],stdout=sub.PIPE)
+    	sub.Popen(['git','add',_oldversion],stdout=sub.PIPE)
+    	sub.call('git commit -m "Added old version %s"'%_oldversion,shell=True)  
 
-
-		def new_commit(_oldversion,_version):
-			move_version()
-			sub.call(['cp -r',self._version,self._oldversion])
-			sub.call(['git add',self._version])
-			sub.run(['git commit'],shell=true)
-		
+    if __name__ == '__main__':
+    	_oldversion= sys.argv[1]
+    	_version = sys.argv[2]
+    	move(_oldversion,_version)
+    	copyadd(_oldversion,_version)
