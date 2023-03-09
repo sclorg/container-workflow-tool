65b93ca0d5cd05f5d039e95c96b2088c774a2174
Petr Hracek <phracek@redhat.com>
2023-03-09 10:10:46 +0100

    Update container_workflow_tool/git_operations.py
    Co-authored-by: Lumír 'Frenzy' Balhar <frenzy.madness@gmail.com>

--- container_workflow_tool/git_operations.py
+++ container_workflow_tool/git_operations.py
@@ -180,7 +180,7 @@ class GitOperations(object):
         os_name = "fedora"
         if self.conf.image_names == "RHEL8":
             os_name = "rhel8"
-        if self.conf.image_names == "RHEL9":
+        elif self.conf.image_names == "RHEL9":
             os_name = "rhel9"
         elif self.conf.image_names == "RHSCL":
             os_name = "rhel7"
